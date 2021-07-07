6e8a2aef140e195a42a94def441a829464269fc3
samueldomdey <samuel.domdey@gmail.com>
2021-07-07 18:59:59 +0200

    0.0.05

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="ezpredict",
-    version="0.0.4",
+    version="0.0.5",
     author="Samuel Domdey",
     author_email="samuel.domdey@gmail.com",
     description="A small package for convenient predictions on fine-tuned huggingface-models",
